40541c1951af79619ac2cf67a1cfd6874bddc7c9
sveta <svetlanakostikova4@gmail.com>
2022-10-03 17:42:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# campaign-survey-catalog
---
+++ campaign-survey-catalog-web-api/src/main/java/com/crowdsourcing/campaign/survey/application/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.crowdsourcing.campaign.survey.api.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static java.util.Collections.singletonList;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.crowdsourcing.campaign.survey"))
+                .build()
+                // add security configuration
+                .globalOperationParameters(singletonList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(true)
+                                .defaultValue("Bearer " + "<access token value>")
+                                .build()
+                        )
+                );
+    }
+
+}
